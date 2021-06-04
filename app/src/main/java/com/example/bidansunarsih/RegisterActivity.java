2845104e58bf90021c774474507a059122a35985
Akhlakul Karim HBP <akhlakulkarimhbp110214@gmail.com>
2021-06-04 18:55:43 +0700

    hallo

--- app/src/main/java/com/example/bidansunarsih/RegisterActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.bidansunarsih;
-
-import android.app.Activity;
-
-public class RegisterActivity extends Activity {
-}
--- app/src/main/java/com/example/bidansunarsih/splash_screen.java
+++ app/src/main/java/com/example/bidansunarsih/splash_screen.java
@@ -1,7 +1,30 @@
 package com.example.bidansunarsih;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
-public class splash_screen extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
 
-}
+public class splash_screen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+        if (getSupportActionBar() != null){
+            getSupportActionBar().hide();
+        }
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(new Intent(splash_screen.this, MainActivity.class));
+                        finish();
+                    }
+                }, 1000);
+        }
+    }
+
